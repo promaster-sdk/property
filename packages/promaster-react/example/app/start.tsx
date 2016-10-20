cecbf81fc7a178209c39d5bb35e9d9a403273be4
Jonas Kello <jonas.kello@divid.se>
2016-10-20 13:47:00 +0200

    Work on example

--- packages/promaster-react/example/app/start.tsx
+++ packages/promaster-react/example/app/start.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {AmountInputBox} from "promaster-react/amount-fields/index";
-//import {Units, Amount} from "promaster-primitives";
-import {Amount} from "promaster-primitives/lib/src/class/measure/amount";
-import {Units} from "promaster-primitives/lib/src/class/measure/unit/units";
-
-console.log("AmountFields", AmountInputBox);
-console.log("Amount", Amount);
-console.log("Units", Units);
-//console.log("Olle", Olle);
+import {Units, Amount} from "promaster-primitives/lib/classes";
 
 ReactDOM.render(
 	<h1>Hello, world!<AmountFieldExample1 />  </h1>,
 	document.getElementById('root')
 );
-
-// ReactDOM.render(
-// 	React.createElement('div', {}, "HELLO!!"),
-// 	document.getElementById('root')
-// );
-
-console.log("hello!!");
-
 
 function AmountFieldExample1() {
 
@@ -31,7 +16,7 @@ function AmountFieldExample1() {
 				isRequiredMessage="Is required"
 				classNames={{input: "input", inputInvalid: "inputInvalid"}}
 				errorMessage="This is the error message"
-				inputDecimalCount={2}
+				inputDecimalCount={3}
 				inputUnit={Units.Celsius}
 				notNumericMessage="Not numeric"
 				onValueChange={() => null}
