a5574a665f992c3e83b8be1686c6d6852e7d8cd0
Jonas Kello <jonas.kello@divid.se>
2016-10-21 22:46:16 +0200

    Editorconfig for tsx

--- packages/promaster-react/example/app/amount-format-selector-example-1.tsx
+++ packages/promaster-react/example/app/amount-format-selector-example-1.tsx
@@ -4,53 +4,60 @@ import {Unit, Units, Amount} from "promaster-primitives";
 import {merge} from "./utils";
 
 interface State {
-    readonly selectedUnit: Unit.Unit<any>;
+  readonly selectedUnit: Unit.Unit<any>,
+  readonly selectedDecimalCount: number,
+  readonly amount: Amount.Amount<any>
 }
 
 export class AmountFormatSelectorExample1 extends React.Component<{}, State> {
 
-    constructor() {
-        this.state = {selectedUnit: Units.Celsius};
-    }
+  constructor() {
+    super();
+    this.state = {
+      selectedUnit: Units.Celsius,
+      amount: Amount.create(10.0, Units.Celsius),
+      selectedDecimalCount: 2,
+    };
+  }
 
-    render() {
+  render() {
 
-        const boxClassNames = {
-            input: "input",
-            inputInvalid: "inputInvalid"
-        };
+    const boxClassNames = {
+      input: "input",
+      inputInvalid: "inputInvalid"
+    };
 
-        const selectorClassNames = {
-            format: "format",
-            formatActive: "formatActive",
-            unit: "unit",
-            precision: "precision",
-            cancel: "cancel"
-        };
+    const selectorClassNames = {
+      format: "format",
+      formatActive: "formatActive",
+      unit: "unit",
+      precision: "precision",
+      cancel: "cancel"
+    };
 
-        return (
-            <div>
-                <div>
-                    AmountFormatSelector:
-                </div>
-                <div>
-                    <AmountInputBox isRequiredMessage="Is required"
-                                    classNames={boxClassNames}
-                                    errorMessage=""
-                                    inputDecimalCount={3}
-                                    inputUnit={Units.Celsius}
-                                    notNumericMessage="Not numeric"
-                                    onValueChange={() => console.log("Value changed.")}
-                                    readOnly={false}
-                                    value={Amount.create(10.0, Units.Celsius)}/>
-                    <AmountFormatSelector classNames={selectorClassNames}
-                                          selectedUnit={this.state.selectedUnit}
-                                          selectedDecimalCount={2}
-                                          onFormatChanged={(u) => this.setState(merge(this.state, {selectedUnit: u}))}/>
-                </div>
-            </div>
-        );
+    return (
+      <div>
+        <div>
+          AmountFormatSelector:
+        </div>
+        <div>
+          <AmountInputBox isRequiredMessage="Is required"
+                          classNames={boxClassNames}
+                          errorMessage=""
+                          inputDecimalCount={this.state.selectedDecimalCount}
+                          inputUnit={this.state.selectedUnit}
+                          notNumericMessage="Not numeric"
+                          onValueChange={(amount) => this.setState(merge(this.state, {amount}))}
+                          readOnly={false}
+                          value={this.state.amount}/>
+          <AmountFormatSelector classNames={selectorClassNames}
+                                selectedUnit={this.state.selectedUnit}
+                                selectedDecimalCount={this.state.selectedDecimalCount}
+                                onFormatChanged={(selectedUnit) => this.setState(merge(this.state, {selectedUnit}))}/>
+        </div>
+      </div>
+    );
 
-    }
+  }
 }
 
