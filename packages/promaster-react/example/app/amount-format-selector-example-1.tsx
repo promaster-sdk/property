005121f22721cbb4f93fc2982162256552fee24e
Jonas Kello <jonas.kello@divid.se>
2016-10-21 17:11:39 +0200

    Example

--- packages/promaster-react/example/app/amount-format-selector-example-1.tsx
+++ packages/promaster-react/example/app/amount-format-selector-example-1.tsx
@@ -1,21 +1,36 @@
 import * as React from "react";
-import {AmountInputBox} from "promaster-react/amount-fields/index";
+import {AmountInputBox, AmountFormatSelector} from "promaster-react/amount-fields/index";
 import {Units, Amount} from "promaster-primitives";
 
 export function AmountFormatSelectorExample1() {
 
+    const classNames = {
+        format: "format",
+        formatActive: "formatActive",
+        unit: "unit",
+        precision: "precision",
+        cancel: "cancel"
+    };
+
     return (
         <div>
-            Mitt fina amount field 22222!
-            <AmountInputBox isRequiredMessage="Is required"
-                            classNames={{input: "input", inputInvalid: "inputInvalid"}}
-                            errorMessage=""
-                            inputDecimalCount={3}
-                            inputUnit={Units.Celsius}
-                            notNumericMessage="Not numeric"
-                            onValueChange={() => console.log("Value changed.")}
-                            readOnly={false}
-                            value={Amount.create(10.0, Units.Celsius)}/>
+            <div>
+                AmountFormatSelector:
+            </div>
+            <div>
+                <AmountInputBox isRequiredMessage="Is required"
+                                classNames={{input: "input", inputInvalid: "inputInvalid"}}
+                                errorMessage=""
+                                inputDecimalCount={3}
+                                inputUnit={Units.Celsius}
+                                notNumericMessage="Not numeric"
+                                onValueChange={() => console.log("Value changed.")}
+                                readOnly={false}
+                                value={Amount.create(10.0, Units.Celsius)}/>
+                <AmountFormatSelector classNames={classNames}
+                                      selectedUnit={Units.Celsius}
+                                      selectedDecimalCount={2}/>
+            </div>
         </div>
     );
 
