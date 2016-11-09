4e3e20268000a3cfd1e34ea055523105251ba9f7
Jonas Kello <jonas.kello@divid.se>
2016-11-09 14:46:20 +0100

    Add compiled files

---
+++ packages/promaster-react/lib/utils/insert-css.d.ts
@@ -0,0 +1,1 @@
+export declare const insertCss: (a: any) => any;
---
+++ packages/promaster-react/lib/utils/insert-css.js
@@ -0,0 +1,4 @@
+"use strict";
+var insert_css_1 = require("insert-css");
+exports.insertCss = function (a) { return insert_css_1.insertCss(a); };
+//# sourceMappingURL=insert-css.js.map
