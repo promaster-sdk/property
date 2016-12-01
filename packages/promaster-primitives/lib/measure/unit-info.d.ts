65c2b655888a70317de54d870909497b12d8b7c4
AdaLuo <adam.luotonen@divid.se>
2016-12-01 13:59:21 +0100

    unit info

--- packages/promaster-primitives/lib/measure/unit-info.d.ts
+++ packages/promaster-primitives/lib/measure/unit-info.d.ts
@@ -1,5 +1,7 @@
 import * as Unit from "./unit";
+export declare type MeasureSystem = "SI" | "IP";
 export interface UnitInfo {
+    readonly measureSystem: MeasureSystem | undefined;
     readonly decimalCount: number;
     readonly coUnit?: Unit.Unit<any>;
 }
