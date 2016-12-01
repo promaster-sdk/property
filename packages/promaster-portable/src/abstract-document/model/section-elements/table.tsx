a4d510ef215a9c0d256d90748e9e7b805d1848c1
Jonas Kello <jonas.kello@divid.se>
2016-12-01 12:20:37 +0100

    WIP: AbstractDoc

--- packages/promaster-portable/src/abstract-document/model/section-elements/table.tsx
+++ packages/promaster-portable/src/abstract-document/model/section-elements/table.tsx
@@ -1,6 +1,6 @@
-import {TableRow} from "../table/types";
 import {TableCellProperties} from "../properties/table-cell-properties";
 import {TableProperties} from "../properties/table-properties";
+import {TableRow} from "../table/table-row";
 
 export interface Table {
   columnWidths: number[];
--- packages/promaster-portable/src/abstract-document/model/table/table-cell.tsx
+++ packages/promaster-portable/src/abstract-document/model/table/table-cell.tsx
@@ -1,14 +1,9 @@
-import {ISectionElement} from "../section-elements/i-section-element";
 import {TableCellProperties} from "../properties/table-cell-properties";
+import {ISectionElement} from "../section-elements/i-section-element";
 
 export interface TableCell {
   columnSpan: number;
   elements: ISectionElement[];
   styleName: string;
   tableCellProperties: TableCellProperties;
-}
-
-export interface TableRow {
-  cells: TableCell[];
-  height: number;
 }
