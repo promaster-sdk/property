b8e66d157ecce82586931dba9cc40fdbe4568f6a
AdaLuo <adam.luotonen@divid.se>
2016-12-01 13:29:33 +0100

    unit info and quantity info

--- packages/promaster-primitives/src/index.ts
+++ packages/promaster-primitives/src/index.ts
@@ -1,9 +1,11 @@
 // measure
 import * as Amount from "./measure/amount";
 import * as Quantity from "./measure/quantity";
+import * as QuantityInfo from "./measure/quantity-info";
 import * as Unit from "./measure/unit";
 import * as Units from "./measure/units";
 import * as UnitName from "./measure/unit-name";
+import * as UnitInfo from "./measure/unit-info";
 import * as UnitDivide from "./measure/unit-divide";
 import * as UnitTimes from "./measure/unit-times";
 // property-filtering
@@ -17,9 +19,11 @@ export {
     // measure
     Amount,
     Quantity,
+    QuantityInfo,
     Unit,
     Units,
     UnitName,
+    UnitInfo,
     UnitDivide,
     UnitTimes,
     // property-filtering
