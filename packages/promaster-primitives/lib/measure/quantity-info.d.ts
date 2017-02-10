6dc0dc4dd84219e0274d712d314a9ef90fdae353
jonathanmourtada <jonathan.mourtada@divid.se>
2017-02-10 13:40:48 +0100

    removed lib from git

--- packages/promaster-primitives/lib/measure/quantity-info.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Unit from "./unit";
-import * as Quantity from "./quantity";
-export declare type QuantityInfo = {
-    readonly siUnit: Unit.Unit<any>;
-    readonly ipUnit: Unit.Unit<any>;
-};
-export declare function addQuantity(quantity: Quantity.Quantity, siUnit: Unit.Unit<any>, ipUnit: Unit.Unit<any>): void;
-export declare function getQuantityInfo(quantity: Quantity.Quantity): QuantityInfo | undefined;
--- packages/promaster-primitives/lib/measure/unit-name.d.ts
+++
@@ -1,4 +0,0 @@
-import { Quantity } from "./quantity";
-import * as Unit from "./unit";
-export declare function registerLabel<T extends Quantity>(label: string, unit: Unit.Unit<T>): void;
-export declare function getName<T extends Quantity>(unit: Unit.Unit<T>): string;
