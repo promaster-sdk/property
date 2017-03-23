f84b90e665ae1eeeb1128c5f687439b0165a7147
Jonas Kello <jonas.kello@divid.se>
2017-03-23 09:04:30 +0100

    Simplify example

--- packages/promaster-react/example/src/properties-selector-example-1.tsx
+++ packages/promaster-react/example/src/properties-selector-example-1.tsx
@@ -1,14 +1,10 @@
 import * as React from "react";
-import * as R from "ramda";
 import { PropertiesSelector } from "@promaster/promaster-react";
-import { Unit, PropertyValueSet } from "@promaster/promaster-primitives";
-import { merge } from "./utils";
+import { PropertyValueSet } from "@promaster/promaster-primitives";
 import { exampleProductProperties } from "./example-product-properties";
 
 interface State {
   readonly propertyValueSet: PropertyValueSet.PropertyValueSet
-  readonly closedGroups: Array<string>,
-  readonly propertyFormats: { [key: string]: PropertiesSelector.AmountFormat },
 }
 
 export class PropertiesSelectorExample1 extends React.Component<{}, State> {
@@ -17,8 +13,6 @@ export class PropertiesSelectorExample1 extends React.Component<{}, State> {
     super();
     this.state = {
       propertyValueSet: PropertyValueSet.fromString("a=10:Celsius;b=1;"),
-      closedGroups: [],
-      propertyFormats: {},
     };
   }
 
@@ -30,19 +24,9 @@ export class PropertiesSelectorExample1 extends React.Component<{}, State> {
       selectedProperties: this.state.propertyValueSet,
       includeCodes: true,
       onChange: (properties: PropertyValueSet.PropertyValueSet) => {
-        this.setState(merge(this.state, { propertyValueSet: properties }))
+        this.setState({ ...this.state, propertyValueSet: properties });
         console.log("updated");
       },
-      onPropertyFormatChanged: (propertyName: string, unit: Unit.Unit<any>, decimalCount: number) =>
-        this.setState(merge(this.state, {
-          propertyFormats: merge(this.state.propertyFormats, {
-            [propertyName]: { unit, decimalCount }
-          })
-        })),
-      onPropertyFormatCleared: (propertyName: string) =>
-        this.setState(merge(this.state, {
-          propertyFormats: R.dissoc(propertyName, this.state.propertyFormats)
-        })),
     };
 
     return (
