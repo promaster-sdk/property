af81d65a785aab437e0d5672f07f1adc97283f20
Jonas Kello <jonas.kello@divid.se>
2017-06-07 09:30:47 +0200

    More autofix

--- packages/promaster-primitives/test/measure/amount-test.ts
+++ packages/promaster-primitives/test/measure/amount-test.ts
@@ -1,11 +1,11 @@
-import {assert, expect} from "chai";
-import {LengthConversion} from "../test_utils/conversion_helpers/length_conversion";
-import {DurationConversion} from "../test_utils/conversion_helpers/duration_conversion";
+import { assert, expect } from "chai";
+import { LengthConversion } from "../test_utils/conversion_helpers/length_conversion";
+import { DurationConversion } from "../test_utils/conversion_helpers/duration_conversion";
 import * as Amount from "../../src/measure/amount";
 import * as Units from "../../src/measure/units";
-import {Mass, Length, Energy, Temperature, Duration} from "../../src/measure/quantity";
+import { Mass, Length, Energy, Temperature, Duration } from "../../src/measure/quantity";
 
-describe('amount_test', () => {
+describe("amount_test", () => {
 
 	/*
 	 it('hash_should_be_same_for_same_value', () => {
@@ -17,126 +17,126 @@ describe('amount_test', () => {
 	 });
 	 */
 
-	it('should_not_accept_a_string_as_value', () => {
-		const a: any = "12.3";
-		expect(() => Amount.create(a, Units.Celsius)).to.throw();
-	});
+  it("should_not_accept_a_string_as_value", () => {
+    const a: any = "12.3"; //tslint:disable-line
+    expect(() => Amount.create(a, Units.Celsius)).to.throw();
+  });
 
-	it('should_not_accept_a_string_as_unit', () => {
-		const a: any = "Celsius";
-		expect(() => Amount.create(12.3, a)).to.throw();
-	});
+  it("should_not_accept_a_string_as_unit", () => {
+    const a: any = "Celsius"; //tslint:disable-line
+    expect(() => Amount.create(12.3, a)).to.throw();
+  });
 
-	it('should_not_accept_a_string_as_decimalCount', () => {
-		const a: any = "12";
-		expect(() => Amount.create(12.3, Units.Celsius, a)).to.throw();
-	});
+  it("should_not_accept_a_string_as_decimalCount", () => {
+    const a: any = "12";  //tslint:disable-line
+    expect(() => Amount.create(12.3, Units.Celsius, a)).to.throw();
+  });
 
-	it('Multiply_double_to_amount', () => {
-		const valueLeft: number = 2.0;
-		const valueRight: number = 5.5;
-		const amountLeft = Amount.create(valueLeft, Units.Gram);
-		const res1 = Amount.times(amountLeft, valueRight);
-		assert.closeTo(valueLeft * valueRight, Amount.valueAs(Units.Gram, res1), 0.000001);
-	});
+  it("Multiply_double_to_amount", () => {
+    const valueLeft: number = 2.0;
+    const valueRight: number = 5.5;
+    const amountLeft = Amount.create(valueLeft, Units.Gram);
+    const res1 = Amount.times(amountLeft, valueRight);
+    assert.closeTo(valueLeft * valueRight, Amount.valueAs(Units.Gram, res1), 0.000001);
+  });
 
-	it('Divide_amount_by_double', () => {
-		const valueLeft: number = 55.39;
-		const valueRight: number = 58.456;
-		const amountLeft = Amount.create(valueLeft, Units.Inch);
-		const res1 = Amount.divide(amountLeft, valueRight);
-		assert.closeTo(valueLeft / valueRight, Amount.valueAs(Units.Inch, res1), 0.00001);
-	});
+  it("Divide_amount_by_double", () => {
+    const valueLeft: number = 55.39;
+    const valueRight: number = 58.456;
+    const amountLeft = Amount.create(valueLeft, Units.Inch);
+    const res1 = Amount.divide(amountLeft, valueRight);
+    assert.closeTo(valueLeft / valueRight, Amount.valueAs(Units.Inch, res1), 0.00001);
+  });
 
-	it('Subtract_amounts_with_same_unit', () => {
-		const valueLeft: number = 0.8;
-		const valueRight: number = 99.56;
-		const amountLeft = Amount.create(valueLeft, Units.Kilojoule);
-		const amountRight = Amount.create(valueRight, Units.Kilojoule);
-		const res1 = Amount.minus(amountLeft, amountRight);
-		assert.closeTo(valueLeft - valueRight, Amount.valueAs(Units.Kilojoule, res1), 0.00001);
-	});
+  it("Subtract_amounts_with_same_unit", () => {
+    const valueLeft: number = 0.8;
+    const valueRight: number = 99.56;
+    const amountLeft = Amount.create(valueLeft, Units.Kilojoule);
+    const amountRight = Amount.create(valueRight, Units.Kilojoule);
+    const res1 = Amount.minus(amountLeft, amountRight);
+    assert.closeTo(valueLeft - valueRight, Amount.valueAs(Units.Kilojoule, res1), 0.00001);
+  });
 
-	it('Add_amounts_with_same_unit', () => {
-		const valueLeft: number = 12.8;
-		const valueRight: number = 10.0;
-		const amountLeft = Amount.create(valueLeft, Units.Inch);
-		const amountRight = Amount.create(valueRight, Units.Inch);
-		const res1 = Amount.plus(amountLeft, amountRight);
-		assert.closeTo(valueLeft + valueRight, Amount.valueAs(Units.Inch, res1), 0.0001);
-	});
+  it("Add_amounts_with_same_unit", () => {
+    const valueLeft: number = 12.8;
+    const valueRight: number = 10.0;
+    const amountLeft = Amount.create(valueLeft, Units.Inch);
+    const amountRight = Amount.create(valueRight, Units.Inch);
+    const res1 = Amount.plus(amountLeft, amountRight);
+    assert.closeTo(valueLeft + valueRight, Amount.valueAs(Units.Inch, res1), 0.0001);
+  });
 
-	it('Add_amounts_with_different_units', () => {
-		const valueLeft: number = 2.0;
-		const valueRight: number = 5.5;
-		const amountLeft = Amount.create(valueLeft, Units.Inch);
-		const amountRight = Amount.create(valueRight, Units.CentiMeter);
-		const res1 = Amount.plus(amountLeft, amountRight);
-		assert.closeTo(valueLeft + LengthConversion.Cm2In(valueRight), Amount.valueAs(Units.Inch, res1), 0.0001);
-	});
+  it("Add_amounts_with_different_units", () => {
+    const valueLeft: number = 2.0;
+    const valueRight: number = 5.5;
+    const amountLeft = Amount.create(valueLeft, Units.Inch);
+    const amountRight = Amount.create(valueRight, Units.CentiMeter);
+    const res1 = Amount.plus(amountLeft, amountRight);
+    assert.closeTo(valueLeft + LengthConversion.Cm2In(valueRight), Amount.valueAs(Units.Inch, res1), 0.0001);
+  });
 
-	it('Subtract_amounts_with_different_units', () => {
-		const valueLeft: number = 360.0;
-		const valueRight: number = 2.0;
-		const amountLeft = Amount.create(valueLeft, Units.Second);
-		const amountRight = Amount.create(valueRight, Units.Hour);
-		const res1 = Amount.minus(amountLeft, amountRight);
-		assert.closeTo(valueLeft - DurationConversion.H2S(valueRight), Amount.valueAs(Units.Second, res1), 0.00001);
-	});
+  it("Subtract_amounts_with_different_units", () => {
+    const valueLeft: number = 360.0;
+    const valueRight: number = 2.0;
+    const amountLeft = Amount.create(valueLeft, Units.Second);
+    const amountRight = Amount.create(valueRight, Units.Hour);
+    const res1 = Amount.minus(amountLeft, amountRight);
+    assert.closeTo(valueLeft - DurationConversion.H2S(valueRight), Amount.valueAs(Units.Second, res1), 0.00001);
+  });
 
-	it('Multiply_amount_to_double', () => {
-		const valueLeft: number = 2.0;
-		const valueRight: number = 5.5;
-		const amountRight = Amount.create(valueRight, Units.Hour);
-		const res1 = Amount.times(amountRight, valueLeft);
-		assert.equal(valueLeft * valueRight, Amount.valueAs(Units.Hour, res1));
-	});
+  it("Multiply_amount_to_double", () => {
+    const valueLeft: number = 2.0;
+    const valueRight: number = 5.5;
+    const amountRight = Amount.create(valueRight, Units.Hour);
+    const res1 = Amount.times(amountRight, valueLeft);
+    assert.equal(valueLeft * valueRight, Amount.valueAs(Units.Hour, res1));
+  });
 
-	it('Create_Amount_And_Check_Explicit_Conversion', () => {
-		const valueLeft: number = -568.25;
-		const valueRight: number = 15.369852;
-		const amountLeft = Amount.create(valueLeft, Units.Second);
-		const amountRight = Amount.create(valueRight, Units.Hour);
-		assert.closeTo(valueLeft, Amount.valueAs(Units.Second, amountLeft), 0.0001);
-		assert.closeTo(valueRight, Amount.valueAs(Units.Hour, amountRight), 0.0001);
-	});
+  it("Create_Amount_And_Check_Explicit_Conversion", () => {
+    const valueLeft: number = -568.25;
+    const valueRight: number = 15.369852;
+    const amountLeft = Amount.create(valueLeft, Units.Second);
+    const amountRight = Amount.create(valueRight, Units.Hour);
+    assert.closeTo(valueLeft, Amount.valueAs(Units.Second, amountLeft), 0.0001);
+    assert.closeTo(valueRight, Amount.valueAs(Units.Hour, amountRight), 0.0001);
+  });
 
-	it('0.8 Celisius should equal 0.8 Celsius', () => {
-		const valueLeft = Amount.create(0.8, Units.Celsius);
-		const valueRight = Amount.create(0.8, Units.Celsius);
+  it("0.8 Celisius should equal 0.8 Celsius", () => {
+    const valueLeft = Amount.create(0.8, Units.Celsius);
+    const valueRight = Amount.create(0.8, Units.Celsius);
 
-		assert.equal(Amount.equals(valueLeft, valueRight), true);
-	});
+    assert.equal(Amount.equals(valueLeft, valueRight), true);
+  });
 
-	it('should equal 1 m with 100 cm', () => {
-		const valueLeft = Amount.create(1, Units.Meter);
-		const valueRight = Amount.create(100, Units.CentiMeter);
-		assert.equal(true, Amount.equals(valueLeft, valueRight));
-	});
+  it("should equal 1 m with 100 cm", () => {
+    const valueLeft = Amount.create(1, Units.Meter);
+    const valueRight = Amount.create(100, Units.CentiMeter);
+    assert.equal(true, Amount.equals(valueLeft, valueRight));
+  });
 
-	it('Operator_GreaterThan', () => {
-		const valueLeft = Amount.create(0.9, Units.Celsius);
-		const valueRight = Amount.create(0.8, Units.Celsius);
-		assert.equal(true, Amount.greaterThan(valueLeft, valueRight));
-	});
+  it("Operator_GreaterThan", () => {
+    const valueLeft = Amount.create(0.9, Units.Celsius);
+    const valueRight = Amount.create(0.8, Units.Celsius);
+    assert.equal(true, Amount.greaterThan(valueLeft, valueRight));
+  });
 
-	it('Operator_LessThan', () => {
-		const valueLeft = Amount.create(0.8, Units.Celsius);
-		const valueRight = Amount.create(0.9, Units.Celsius);
-		assert.equal(true, Amount.lessThan(valueLeft, valueRight));
-	});
+  it("Operator_LessThan", () => {
+    const valueLeft = Amount.create(0.8, Units.Celsius);
+    const valueRight = Amount.create(0.9, Units.Celsius);
+    assert.equal(true, Amount.lessThan(valueLeft, valueRight));
+  });
 
-	it('Operator_LessThanOrEquals', () => {
-		const valueLeft = Amount.create(0.8, Units.Celsius);
-		const valueRight = Amount.create(0.9, Units.Celsius);
-		assert.equal(true, Amount.lessOrEqualTo(valueLeft, valueRight));
-	});
+  it("Operator_LessThanOrEquals", () => {
+    const valueLeft = Amount.create(0.8, Units.Celsius);
+    const valueRight = Amount.create(0.9, Units.Celsius);
+    assert.equal(true, Amount.lessOrEqualTo(valueLeft, valueRight));
+  });
 
-	it('Operator_GreaterThanOrEquals', () => {
-		const valueLeft = Amount.create(0.9, Units.Celsius);
-		const valueRight = Amount.create(0.8, Units.Celsius);
-		assert.equal(true, Amount.greaterOrEqualTo(valueLeft, valueRight));
-	});
+  it("Operator_GreaterThanOrEquals", () => {
+    const valueLeft = Amount.create(0.9, Units.Celsius);
+    const valueRight = Amount.create(0.8, Units.Celsius);
+    assert.equal(true, Amount.greaterOrEqualTo(valueLeft, valueRight));
+  });
 
 	/*
 	 it('Tolerance_Result_Of_Substraction_Equals_Constant', () => {
@@ -148,32 +148,32 @@ describe('amount_test', () => {
 	 });
 	 */
 
-	it('Assert true for 16.2 Celsius > -200.0 Celsius', () => {
-		const left = Amount.create(16.2, Units.Celsius);
-		const right = Amount.create(-200.0, Units.Celsius);
-		assert.equal(Amount.greaterThan(left, right), true);
-	});
+  it("Assert true for 16.2 Celsius > -200.0 Celsius", () => {
+    const left = Amount.create(16.2, Units.Celsius);
+    const right = Amount.create(-200.0, Units.Celsius);
+    assert.equal(Amount.greaterThan(left, right), true);
+  });
 
-	it('Assert true for -200.0 Celsius < 16.2 Celsius', () => {
-		const left = Amount.create(-200.0, Units.Celsius);
-		const right = Amount.create(16.2, Units.Celsius);
-		assert.equal(Amount.lessThan(left, right), true);
-	});
+  it("Assert true for -200.0 Celsius < 16.2 Celsius", () => {
+    const left = Amount.create(-200.0, Units.Celsius);
+    const right = Amount.create(16.2, Units.Celsius);
+    assert.equal(Amount.lessThan(left, right), true);
+  });
 
-  it('Assert true for 0 CubicMeterPerSecond < 36 CubicMeterPerHour', () => {
+  it("Assert true for 0 CubicMeterPerSecond < 36 CubicMeterPerHour", () => {
     const left = Amount.create(0, Units.CubicMeterPerSecond);
     const right = Amount.create(36, Units.CubicMeterPerHour);
     assert.equal(Amount.lessThan(left, right), true);
   });
 
-  it('Assert 32.5 CubicMeterPerHour is larger than 9 LiterPerSecond', () => {
+  it("Assert 32.5 CubicMeterPerHour is larger than 9 LiterPerSecond", () => {
     const a1 = Amount.create(32.5, Units.CubicMeterPerHour);
     const a2 = Amount.create(9, Units.LiterPerSecond);
     const max = Amount.max(a1, a2);
     assert.equal(max, a1);
   });
 
-  it('Assert 33 CubicMeterPerHour is less than 9.2 LiterPerSecond', () => {
+  it("Assert 33 CubicMeterPerHour is less than 9.2 LiterPerSecond", () => {
     const a1 = Amount.create(33, Units.CubicMeterPerHour);
     const a2 = Amount.create(9.2, Units.LiterPerSecond);
     const max = Amount.min(a1, a2);
@@ -262,7 +262,6 @@ describe('amount_test', () => {
 	 assert.equal(compare, 0);
 	 });
 
-
 	 it('Test_Amount_Compare_Normal10_Normal_20_Negative', () => {
 	 Amount<IMassFlow> a = new Amount< IMassFlow > .exact (-Normal10, Units.KilogramPerSecond);
 	 Amount<IMassFlow> b = new Amount< IMassFlow > .exact (-Normal20, Units.KilogramPerSecond);
@@ -299,7 +298,6 @@ describe('amount_test', () => {
 	 assert.equal(compare, 0);
 	 });
 
-
 	 it('Test_Amount_Compare_Zero0_Zero0_Negative', () => {
 	 Amount<IMassFlow> a = new Amount<IMassFlow>.exact (-zero0, Units.KilogramPerSecond);
 	 Amount<IMassFlow> b = new Amount<IMassFlow>.exact (-zero0, Units.KilogramPerSecond);
@@ -308,7 +306,6 @@ describe('amount_test', () => {
 	 assert.equal(compare, 0);
 	 });
 
-
 	 it('Test_Amount_Compare_Zero1_Zero2_Positive', () => {
 	 Amount<IMassFlow> a = new Amount<IMassFlow>.exact (zero1, Units.KilogramPerSecond);
 	 Amount<IMassFlow> b = new Amount<IMassFlow>.exact (zero2, Units.KilogramPerSecond);
@@ -316,7 +313,6 @@ describe('amount_test', () => {
 	 int compare = a.compareTo(b);
 	 assert.equal(compare, isNot(0));
 	 });
-
 
 	 it('Test_Amount_Compare_Zero1_Zero2_Negative', () => {
 	 Amount<IMassFlow> a = new Amount<IMassFlow>.exact (-zero1, Units.KilogramPerSecond);
--- packages/promaster-primitives/test/product-properties/property-value-test.ts
+++ packages/promaster-primitives/test/product-properties/property-value-test.ts
@@ -1,143 +1,143 @@
-import {assert} from 'chai';
+import {assert} from "chai";
 import * as PropertyValue from "../../src/product-properties/property-value";
 import * as Amount from "../../src/measure/amount";
 import * as Units from "../../src/measure/units";
 import {Quantity} from "../../src/measure/quantity";
 
-describe('property_value_test', () => {
+describe("property_value_test", () => {
 
-  it('should_parse_amount_with_decimal_dot', () => {
+  it("should_parse_amount_with_decimal_dot", () => {
     const pv1 = fromStringOrException("2.1:Celsius");
     const amount1 = getAmountOrException(pv1);
     const amount2 = Amount.create(2.1, Units.Celsius, 1);
     assert.equal(Amount.equals(amount1, amount2), true);
   });
 
-  it('should_parse_integer', () => {
+  it("should_parse_integer", () => {
     const pv1 = fromStringOrException("2");
     assert.equal(PropertyValue.getInteger(pv1), 2);
   });
 
-  it('should_parse_text', () => {
-    const pv1 = fromStringOrException('"Olle"');
+  it("should_parse_text", () => {
+    const pv1 = fromStringOrException("\"Olle\"");
     assert.equal(PropertyValue.getText(pv1), "Olle");
   });
 
-  it('should_do_value_compare_integer_with_equals_method', () => {
+  it("should_do_value_compare_integer_with_equals_method", () => {
     const pv1 = PropertyValue.fromInteger(2);
     const pv2 = PropertyValue.fromInteger(2);
     assert.equal(PropertyValue.equals(pv1, pv2), true);
   });
 
-  it('should_do_value_compare_amount_with_equals_method', () => {
+  it("should_do_value_compare_amount_with_equals_method", () => {
     const pv1 = PropertyValue.fromAmount(Amount.create(2.0, Units.Celsius));
     const pv2 = PropertyValue.fromAmount(Amount.create(2.0, Units.Celsius));
     assert.equal(PropertyValue.equals(pv1, pv2), true);
   });
 
-  it('should_do_value_compare_string_with_equals_method', () => {
+  it("should_do_value_compare_string_with_equals_method", () => {
     const pv1 = PropertyValue.fromText("abcABC");
     const pv2 = PropertyValue.fromText("abcABC");
     assert.equal(PropertyValue.equals(pv1, pv2), true);
   });
 
-  it('should_do_value_compare_integer_with_equality_operator', () => {
+  it("should_do_value_compare_integer_with_equality_operator", () => {
     const pv1 = PropertyValue.fromInteger(2);
     const pv2 = PropertyValue.fromInteger(2);
     assert.equal(PropertyValue.equals(pv1, pv2), true);
   });
 
-  it('should_do_value_compare_amount_with_equality_operator', () => {
+  it("should_do_value_compare_amount_with_equality_operator", () => {
     const pv1 = PropertyValue.fromAmount(Amount.create(2.0, Units.Celsius));
     const pv2 = PropertyValue.fromAmount(Amount.create(2.0, Units.Celsius));
     assert.equal(PropertyValue.equals(pv1, pv2), true);
   });
 
-  it('should_do_value_compare_string_with_equality_operator', () => {
+  it("should_do_value_compare_string_with_equality_operator", () => {
     const pv1 = PropertyValue.fromText("abcABC");
     const pv2 = PropertyValue.fromText("abcABC");
     assert.equal(PropertyValue.equals(pv1, pv2), true);
   });
 
-  it('should_parse_string_in_quotes_same_as_explicit_text_constructor', () => {
+  it("should_parse_string_in_quotes_same_as_explicit_text_constructor", () => {
     const pv1 = fromStringOrException("\"abcABC\"");
     const pv2 = PropertyValue.fromText("abcABC");
     assert.equal(PropertyValue.equals(pv1, pv2), true);
   });
 
-  it('should_parse_amount_string_with_two_decimals_to_amount_with_two_decimals', () => {
+  it("should_parse_amount_string_with_two_decimals_to_amount_with_two_decimals", () => {
     const pv1 = fromStringOrException("12.34:Celsius");
     const amount = getAmountOrException(pv1);
     assert.equal(amount.decimalCount, 2);
   });
 
-  it('should_parse_amount_string_with_one_decimal_to_amount_with_one_decimal', () => {
+  it("should_parse_amount_string_with_one_decimal_to_amount_with_one_decimal", () => {
     const pv1 = fromStringOrException("532.5:Watt");
     const amount = getAmountOrException(pv1);
     assert.equal(amount.decimalCount, 1);
   });
 
-  it('should_parse_amount_string_with_zero_decimals_to_amount_with_zero_decimals', () => {
+  it("should_parse_amount_string_with_zero_decimals_to_amount_with_zero_decimals", () => {
     const pv1 = fromStringOrException("532:Inch");
     const amount = getAmountOrException(pv1);
     assert.equal(amount.decimalCount, 0);
   });
 
-  it('should_parse_empty_string_to_property_value_of_type_text', () => {
+  it("should_parse_empty_string_to_property_value_of_type_text", () => {
     const pv1 = fromStringOrException("");
     assert.equal(pv1.type, "text");
   });
 
-  it('should_give_undefined_if_type_is_not_matching', () => {
+  it("should_give_undefined_if_type_is_not_matching", () => {
     const pv1 = fromStringOrException("10:Celsius");
     assert.equal(PropertyValue.getInteger(pv1), undefined);
   });
 
-  it('should_compare_integers_correctly1', () => {
+  it("should_compare_integers_correctly1", () => {
     const pv1 = fromStringOrException("2");
     const pv2 = fromStringOrException("3");
     assert.equal(PropertyValue.compareTo(pv1, pv2) < 0, true);
   });
 
-  it('should_compare_integers_correctly2', () => {
+  it("should_compare_integers_correctly2", () => {
     const pv2 = fromStringOrException("3");
     const pv3 = fromStringOrException("3");
     assert.equal(PropertyValue.compareTo(pv2, pv3) === 0, true);
   });
 
-  it('should_compare_integers_correctly3', () => {
+  it("should_compare_integers_correctly3", () => {
     const pv1 = fromStringOrException("2");
     const pv3 = fromStringOrException("3");
     assert.equal(PropertyValue.compareTo(pv3, pv1) > 0, true);
   });
 
-  it('should make a correct string from an amount value', () => {
+  it("should make a correct string from an amount value", () => {
     const pv1 = fromStringOrException("20.03:Celsius");
     const pv1string = PropertyValue.toString(pv1);
     assert.equal(pv1string, "20.03:celsius");
   });
 
-  it('should make a correct string from an integer value', () => {
+  it("should make a correct string from an integer value", () => {
     const pv1 = PropertyValue.fromInteger(123);
     const pv1string = PropertyValue.toString(pv1);
     assert.equal(pv1string, "123");
   });
 
-  it('should make a correct string from an text value', () => {
+  it("should make a correct string from an text value", () => {
     const pv1 = PropertyValue.fromText("TextValue");
     const pv1string = PropertyValue.toString(pv1);
-    assert.equal(pv1string, '"TextValue"');
+    assert.equal(pv1string, "\"TextValue\"");
   });
 
-  it('should make a correct string from an text value', () => {
+  it("should make a correct string from an text value", () => {
     const pv1 = PropertyValue.fromText("TextValue");
     const pv1string = PropertyValue.toString(pv1);
-    assert.equal(pv1string, '"TextValue"');
+    assert.equal(pv1string, "\"TextValue\"");
   });
 
-  it('should make an empty string when amount value is null', () => {
+  it("should make an empty string when amount value is null", () => {
     const amount = Amount.create(0, Units.Ampere, 1);
-    (<any>amount).value = null;
+    (amount as any).value = null;  //tslint:disable-line
     const pv1 = PropertyValue.fromAmount(amount);
     const pv1string = PropertyValue.toString(pv1);
     assert.equal(pv1string, "");
@@ -145,7 +145,7 @@ describe('property_value_test', () => {
 
 });
 
-describe('PropertyValue.greaterOrEqualTo', () => {
+describe("PropertyValue.greaterOrEqualTo", () => {
 
   it("should assert false for 0:CubicMeterPerSecond >= 16:CubicMeterPerHour", () => {
     const pv1 = fromStringOrException("0:CubicMeterPerSecond");
