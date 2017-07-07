da574bc493ff73b559f72567fdb3b6f564619e36
Jonas Kello <jonas.kello@divid.se>
2017-07-08 00:08:23 +0200

    Add react-story book (inital setup)

---
+++ packages/promaster-react/.storybook/webpack.config.js
@@ -0,0 +1,41 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
+/*
+module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
+  module: {
+    loaders: [
+      // add your custom loaders.
+    ],
+  },
+};
+*/
+
+// load the default config generator.
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
+  // Extend it as you need.
+
+  // For example, add typescript loader:
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('awesome-typescript-loader'),
+    query: {
+      configFileName: './stories/tsconfig.json'
+    }
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  return config;
+};
