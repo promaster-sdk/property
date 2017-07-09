056683442ecb77a3d2e4a2548d22a33b62eb8d76
Jonas Kello <jonas.kello@divid.se>
2017-07-09 20:08:56 +0200

    All examples converted to stories

--- packages/promaster-react/stories/index.tsx
+++ packages/promaster-react/stories/index.tsx
@@ -14,6 +14,9 @@ import { PropertiesSelectorExample1 } from "./properties-selector/example-1";
 import { PropertiesSelectorExample2 } from "./properties-selector/example-2";
 import { PropertiesSelectorExample3SingleValidValue } from "./properties-selector/example-3-single-valid-value";
 import { PropertiesSelectorExampleEmptyPvs } from "./properties-selector/example-empty-pvs";
+import { ReactSvgExportExample1 } from "./react-svg-export/example-1";
+import { SvgExportExample1 } from "./svg-export/example-1";
+import { TextboxPropertySelectorExample1 } from "./textbox-property-selector/example-1";
 
 storiesOf("Welcome", module).add("to Storybook", () => <Welcome showApp={linkTo("Button")} />);
 
@@ -38,3 +41,12 @@ storiesOf("Properties Selector", module)
   .add("example 2, with custom layout", () => <PropertiesSelectorExample2 />)
   .add("example 3, with single valid value", () => <PropertiesSelectorExample3SingleValidValue />)
   .add("with empty property value set", () => <PropertiesSelectorExampleEmptyPvs />);
+
+storiesOf("React Svg Export", module)
+  .add("example 1", () => <ReactSvgExportExample1 />);
+
+storiesOf("Svg Export (not a component?)", module)
+  .add("example 1", () => <SvgExportExample1 />);
+
+storiesOf("Textbox Property Selector", module)
+  .add("example 1", () => <TextboxPropertySelectorExample1 />);
---
+++ packages/promaster-react/stories/react-svg-export/example-1.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { AbstractImage } from "@promaster/promaster-portable";
+import { AbstractImageExporters } from "@promaster/promaster-react";
+
+export function ReactSvgExportExample1(): React.ReactElement<{}> {
+  const components = [
+    AbstractImage.createLine(AbstractImage.createPoint(25, 25), AbstractImage.createPoint(80, 60), AbstractImage.black, 2),
+    AbstractImage.createRectangle(AbstractImage.createPoint(10, 50), AbstractImage.createPoint(40, 80), AbstractImage.blue, 2, AbstractImage.red),
+    AbstractImage.createText(AbstractImage.createPoint(25, 25),
+      "Testing texting", "Arial", 10, AbstractImage.black, "normal", 0, "left", "uniform", "uniform", 0, AbstractImage.black)
+  ];
+  const image = AbstractImage.createAbstractImage(AbstractImage.createPoint(0, 0), AbstractImage.createSize(400, 400), AbstractImage.white, components);
+  const svg = AbstractImageExporters.createReactSvg(image);
+  return (
+    <div>
+      <h1>React Svg</h1>
+      <pre>{svg}</pre>
+
+    </div>);
+}
