bb22fecc17ce9f10a0683547c54613f4c51b24f5
Jonas Kello <jonas.kello@divid.se>
2017-11-10 10:26:49 +0100

    Exit on error

--- packages/promaster-primitives/scripts/publish.js
+++ packages/promaster-primitives/scripts/publish.js
@@ -1,11 +1,13 @@
-const shell = require('shelljs');
+const shell = require("shelljs");
+
+shell.set("-e");
 
 const cmdArguments = process.argv.slice(2);
 const semverType = cmdArguments[0];
 
-if(!semverType || !semverType.match(/^(major|minor|patch)$/)) {
-    console.log(`Usage: major|minor|patch`);
-    process.exit(1);
+if (!semverType || !semverType.match(/^(major|minor|patch)$/)) {
+  console.log(`Usage: major|minor|patch`);
+  process.exit(1);
 }
 
 execCommand(`node yarn.js run build`);
@@ -16,9 +18,9 @@ execCommand(`git push --tags`);
 execCommand(`npm publish`);
 
 function execCommand(command) {
-    const result = shell.exec(command);
-    if(!!result.code) {
-        console.log(result.stdout);
-        process.exit(1);
-    }
+  const result = shell.exec(command);
+  if (!!result.code) {
+    console.log(result.stdout);
+    process.exit(1);
+  }
 }
