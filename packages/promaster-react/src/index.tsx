22cf2fe7c739ba6e41d3492fc5213b4770056803
AdaLuo <adam.luotonen@divid.se>
2017-11-21 09:34:12 +0100

    export radio group elements

--- packages/promaster-react/src/index.tsx
+++ packages/promaster-react/src/index.tsx
@@ -1,7 +1,8 @@
 import * as AmountFields from "./amount-fields/index";
 import * as ComboBoxElements from "./combo-box-elements/index";
+import * as RadioGroupElements from "./radio-group-elements/index";
 import * as PropertySelectors from "./property-selectors/index";
 import * as PropertiesSelector from "./properties-selector/index";
 import * as AbstractImageExporters from "./abstract-image-exporters/index";
 import * as Dropdown from "./combo-box-elements/index";
-export { AmountFields, ComboBoxElements, PropertySelectors, PropertiesSelector, AbstractImageExporters, Dropdown };
+export { AmountFields, ComboBoxElements, RadioGroupElements, PropertySelectors, PropertiesSelector, AbstractImageExporters, Dropdown };
