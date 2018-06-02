79fee44e25e576b69cd2a44d2c58b609bd990940
Jonas Kello <jonas.kello@divid.se>
2018-06-02 13:08:37 +0200

    Fix imports

--- packages/react-property-selectors/stories/amount-format-selector/example-1.tsx
+++ packages/react-property-selectors/stories/amount-format-selector/example-1.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-// import { AmountFields } from "@promaster/promaster-react";
 import {
   AmountFormatWrapper,
   AmountFormatWrapperProps
--- packages/react-property-selectors/stories/combobox-property-selector/example-1.tsx
+++ packages/react-property-selectors/stories/combobox-property-selector/example-1.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-// import { PropertySelectors as Selectors } from "@promaster/promaster-react";
 import {
   createComboboxPropertySelector,
   ComboBoxPropertyValueItem
