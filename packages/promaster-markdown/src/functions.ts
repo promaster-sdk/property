222c130c19d82f0840b9755cf26d8b2d797dff51
Jonas Kello <jonas.kello@divid.se>
2018-09-22 19:36:50 +0200

    Remove promaster markdown

--- packages/promaster-markdown/src/functions.ts
+++
@@ -1,8 +0,0 @@
-import { Converter, extension } from "showdown";
-import { supsub } from "./extension";
-
-export function makeHtml(text: string): string {
-  extension("subsup", supsub);
-  const converter = new Converter({ extensions: ["subsup"] });
-  return converter.makeHtml(text);
-}
