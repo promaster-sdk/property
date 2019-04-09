d3bd4c601edff9d612273ddcc8d1fdd9390b1de3
Jonathan Mourtada <jonathan.mourtada@divid.se>
2019-04-09 14:06:37 +0200

    Fix sed sourcemapping url

--- scripts/delete-maps.js
+++ scripts/delete-maps.js
@@ -2,7 +2,6 @@ const shell = require("shelljs");
 const glob = require("glob");
 
 shell.rm("-f", "packages/*/lib/**/*.map");
-
-glob("packages/*/lib/{*.d.ts,**/*.d.ts", function(er, files) {
+glob("packages/*/lib/{*.d.ts,**/*.d.ts}", function(er, files) {
   shell.sed("-i", /^\/\/# sourceMappingURL=.*/, "", files);
 });
