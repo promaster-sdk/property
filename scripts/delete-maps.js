9ad1ce83699a5237cb75536beb1e2cc149c1c7d3
Jonas Kello <jonas.kello@gmail.com>
2019-10-18 20:16:50 +0200

    Remove unused scripts

--- scripts/delete-maps.js
+++
@@ -1,7 +0,0 @@
-const shell = require("shelljs");
-const glob = require("glob");
-
-shell.rm("-f", "packages/*/lib/**/*.map");
-glob("packages/*/lib/{*.d.ts,**/*.d.ts}", function(er, files) {
-  shell.sed("-i", /^\/\/# sourceMappingURL=.*/, "", files);
-});
