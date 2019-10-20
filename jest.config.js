56e4ebba3c4a3d1842c7a9ea127733beac274cd4
Jonas Kello <jonas.kello@gmail.com>
2019-10-20 13:18:08 +0200

    projects

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,21 @@
 module.exports = {
-  preset: "ts-jest",
-  // projects: ["<rootDir>/packages/property/jest.config.js"]
-
   projects: [
     {
       displayName: "property",
       preset: "ts-jest",
       testMatch: ["<rootDir>/packages/property/**/*.test.ts"]
+    },
+    {
+      displayName: "react-property-selectors",
+      preset: "ts-jest",
+      testMatch: [
+        "<rootDir>/packages/react-property-selectors/**/*.test.ts?(x)"
+      ],
+      globals: {
+        "ts-jest": {
+          tsConfig: "<rootDir>/packages/react-property-selectors/tsconfig.json"
+        }
+      }
     }
-    // {
-    //   displayName: "module2",
-    //   testMatch: ["<rootDir>/httpdocs/modules/module2/**/*.test.js"]
-    // }
   ]
-
-  // coverageDirectory: "<rootDir>/coverage/",
-  // collectCoverageFrom: ["<rootDir>/packages/*/lib/**/*.{js,jsx}"],
-  // coverageReporters: ["text-summary", "lcov"],
-  // // Run only compiled tests (by default typescript files are matched)
-  // // testMatch: ["**/?(*.)+(spec|test).[j]s?(x)"],
-  // testMatch: ["**/?(*.)+(spec|test).ts?(x)"],
-  // // snapshotResolver: "./jest-snapshot-resolver.js",
-  // roots: [
-  //   "<rootDir>/packages/property/src",
-  //   "<rootDir>/packages/property-filter-pretty/src",
-  //   "<rootDir>/packages/react-properties-selector/src",
-  //   "<rootDir>/packages/react-property-selectors/src",
-  //   "<rootDir>/packages/variant-listing/src"
-  // ]
 };
