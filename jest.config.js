ec88df418ddedb29c9918f08e7360b04c825fb73
Jonas Kello <jonas.kello@gmail.com>
2019-10-20 13:30:46 +0200

    Use ts-jest and projects

--- jest.config.js
+++ jest.config.js
@@ -3,19 +3,56 @@ module.exports = {
     {
       displayName: "property",
       preset: "ts-jest",
+      globals: {
+        "ts-jest": {
+          tsConfig: "<rootDir>/packages/property/tsconfig.json"
+        }
+      },
       testMatch: ["<rootDir>/packages/property/**/*.test.ts"]
+    },
+    {
+      displayName: "property-filter-pretty",
+      preset: "ts-jest",
+      globals: {
+        "ts-jest": {
+          tsConfig: "<rootDir>/packages/property-filter-pretty/tsconfig.json"
+        }
+      },
+      testMatch: ["<rootDir>/packages/property-filter-pretty/**/*.test.ts?(x)"]
+    },
+    {
+      displayName: "react-properties-selector",
+      preset: "ts-jest",
+      globals: {
+        "ts-jest": {
+          tsConfig: "<rootDir>/packages/react-properties-selector/tsconfig.json"
+        }
+      },
+      testMatch: [
+        "<rootDir>/packages/react-properties-selector/**/*.test.ts?(x)"
+      ]
     },
     {
       displayName: "react-property-selectors",
       preset: "ts-jest",
-      testMatch: [
-        "<rootDir>/packages/react-property-selectors/**/*.test.ts?(x)"
-      ],
       globals: {
         "ts-jest": {
           tsConfig: "<rootDir>/packages/react-property-selectors/tsconfig.json"
         }
-      }
+      },
+      testMatch: [
+        "<rootDir>/packages/react-property-selectors/**/*.test.ts?(x)"
+      ]
+    },
+    {
+      displayName: "variant-listing",
+      preset: "ts-jest",
+      globals: {
+        "ts-jest": {
+          tsConfig: "<rootDir>/packages/variant-listing/tsconfig.json"
+        }
+      },
+      testMatch: ["<rootDir>/packages/variant-listing/**/*.test.ts?(x)"]
     }
   ]
 };
