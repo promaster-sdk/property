91be45b74441e2b2357d474a6eb6079908e75bc1
Jonas Kello <jonas.kello@gmail.com>
2019-10-20 15:08:58 +0200

    Coverage

--- packages/property/jest.config.js
+++ packages/property/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   displayName: "property",
   preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.json"
+    }
+  },
   testMatch: ["<rootDir>/src/**/*.test.ts?(x)"]
 };
