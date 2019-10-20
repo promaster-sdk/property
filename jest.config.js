dc4161f48ab627732096d5e1aa6c85ff9a1a0f9f
Jonas Kello <jonas.kello@gmail.com>
2019-10-20 11:37:21 +0200

    Use snapshot resolver

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   collectCoverageFrom: ["<rootDir>/packages/*/lib/**/*.{js,jsx}"],
   coverageReporters: ["text-summary", "lcov"],
   // Run only compiled tests (by default typescript files are matched)
-  testMatch: ["**/?(*.)+(spec|test).[j]s?(x)"]
+  testMatch: ["**/?(*.)+(spec|test).[j]s?(x)"],
+  snapshotResolver: "./jest-snapshot-resolver.js"
 };
