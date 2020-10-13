a9d33f6f8eefe8a7e79e67419abfd4e11c00fd24
Jonas Kello <jonas.kello@gmail.com>
2020-10-13 22:29:04 +0200

    Cleanup

--- packages/react-property-selectors/src/hooks/amount/use-amount-property-selector.tsx
+++ packages/react-property-selectors/src/hooks/amount/use-amount-property-selector.tsx
@@ -76,43 +76,6 @@ export function useAmountPropertySelector(
     propertyValueSet
   );
 
-  // return (
-  //   <AmountPropertySelectorWrapper>
-  //     <AmountInputBox
-  //       value={value}
-  //       inputUnit={inputUnit}
-  //       inputDecimalCount={inputDecimalCount}
-  //       notNumericMessage={notNumericMessage}
-  //       isRequiredMessage={isRequiredMessage}
-  //       errorMessage={_getValidationMessage(
-  //         propertyValueSet,
-  //         value,
-  //         validationFilter,
-  //         filterPrettyPrint,
-  //         comparer
-  //       )}
-  //       readOnly={readOnly}
-  //       onValueChange={(newAmount) =>
-  //         onValueChange(
-  //           newAmount !== undefined
-  //             ? PropertyValue.create("amount", newAmount)
-  //             : undefined
-  //         )
-  //       }
-  //       debounceTime={debounceTime}
-  //     />
-  //     <AmountFormatSelector
-  //       selectedUnit={inputUnit}
-  //       selectedDecimalCount={inputDecimalCount}
-  //       onFormatChanged={onFormatChanged}
-  //       onFormatCleared={onFormatCleared}
-  //       onFormatSelectorActiveChanged={onFormatSelectorToggled}
-  //       unitsFormat={unitsFormat}
-  //       units={units}
-  //     />
-  //   </AmountPropertySelectorWrapper>
-  // );
-
   const amountInputBox = useAmountInputBox({
     value,
     inputUnit,
@@ -121,7 +84,7 @@ export function useAmountPropertySelector(
     isRequiredMessage,
     readonly,
     debounceTime,
-    errorMessage: _getValidationMessage(
+    errorMessage: getValidationMessage(
       propertyValueSet,
       value,
       validationFilter,
@@ -148,137 +111,7 @@ export function useAmountPropertySelector(
   return { getWrapperProps: () => ({}), amountInputBox, amountFormatSelector };
 }
 
-// export interface AmountPropertySelectorProps {
-//   readonly propertyName: string;
-//   readonly propertyValueSet: PropertyValueSet.PropertyValueSet;
-//   readonly inputUnit: Unit.Unit<unknown>;
-//   readonly inputDecimalCount: number;
-//   readonly validationFilter: PropertyFilter.PropertyFilter;
-//   readonly notNumericMessage: string;
-//   readonly isRequiredMessage: string;
-//   readonly filterPrettyPrint: PropertyFiltering.FilterPrettyPrint;
-//   readonly readOnly: boolean;
-//   readonly onFormatChanged: OnFormatChanged;
-//   readonly onFormatCleared: OnFormatCleared;
-//   readonly onFormatSelectorToggled?: OnFormatSelectorToggled;
-//   readonly onValueChange: (
-//     newValue: PropertyValue.PropertyValue | undefined
-//   ) => void;
-//   readonly debounceTime?: number;
-//   readonly fieldName: string;
-//   readonly unitsFormat: {
-//     readonly [key: string]: UnitFormat.UnitFormat;
-//   };
-//   readonly units: {
-//     readonly [key: string]: Unit.Unit<unknown>;
-//   };
-//   readonly comparer?: PropertyValue.Comparer;
-// }
-
-// export type AmountPropertySelector = React.ComponentClass<
-//   AmountPropertySelectorProps
-// >;
-// export interface CreateAmountPropertySelectorProps {
-//   readonly AmountPropertySelectorWrapper?: React.ComponentType<
-//     React.DetailedHTMLProps<
-//       React.HTMLAttributes<HTMLSpanElement>,
-//       HTMLSpanElement
-//     >
-//   >;
-//   readonly AmountFormatSelector?: AmountFormatSelector;
-//   readonly AmountInputBox?: AmountInputBox;
-// }
-
-// const defaultAmountPropertySelectorWrapper = (
-//   props: React.DetailedHTMLProps<
-//     React.HTMLAttributes<HTMLSpanElement>,
-//     HTMLSpanElement
-//   >
-// ): JSX.Element => <span {...props} />;
-
-// const defaultAmountFormatSelector = createAmountFormatSelector({});
-// const defaultAmountInputBox = createAmountInputBox({});
-
-// export function createAmountPropertySelector({
-//   AmountPropertySelectorWrapper = defaultAmountPropertySelectorWrapper,
-//   AmountFormatSelector = defaultAmountFormatSelector,
-//   AmountInputBox = defaultAmountInputBox,
-// }: CreateAmountPropertySelectorProps): AmountPropertySelector {
-//   // eslint-disable-next-line functional/no-class
-//   return class AmountPropertySelector extends React.Component<
-//     AmountPropertySelectorProps,
-//     {}
-//   > {
-//     render(): React.ReactElement<AmountPropertySelectorProps> {
-//       const {
-//         onValueChange,
-//         onFormatChanged,
-//         onFormatCleared,
-//         onFormatSelectorToggled,
-//         notNumericMessage,
-//         isRequiredMessage,
-//         validationFilter,
-//         propertyValueSet,
-//         propertyName,
-//         filterPrettyPrint,
-//         inputUnit,
-//         inputDecimalCount,
-//         readOnly,
-//         debounceTime = 350,
-//         unitsFormat,
-//         units,
-//         comparer = PropertyValue.defaultComparer,
-//         // eslint-disable-next-line functional/no-this-expression
-//       } = this.props;
-
-//       const value:
-//         | Amount.Amount<unknown>
-//         | undefined = PropertyValueSet.getAmount(
-//         propertyName,
-//         propertyValueSet
-//       );
-
-//       return (
-//         <AmountPropertySelectorWrapper>
-//           <AmountInputBox
-//             value={value}
-//             inputUnit={inputUnit}
-//             inputDecimalCount={inputDecimalCount}
-//             notNumericMessage={notNumericMessage}
-//             isRequiredMessage={isRequiredMessage}
-//             errorMessage={_getValidationMessage(
-//               propertyValueSet,
-//               value,
-//               validationFilter,
-//               filterPrettyPrint,
-//               comparer
-//             )}
-//             readOnly={readOnly}
-//             onValueChange={(newAmount) =>
-//               onValueChange(
-//                 newAmount !== undefined
-//                   ? PropertyValue.create("amount", newAmount)
-//                   : undefined
-//               )
-//             }
-//             debounceTime={debounceTime}
-//           />
-//           <AmountFormatSelector
-//             selectedUnit={inputUnit}
-//             selectedDecimalCount={inputDecimalCount}
-//             onFormatChanged={onFormatChanged}
-//             onFormatCleared={onFormatCleared}
-//             onFormatSelectorActiveChanged={onFormatSelectorToggled}
-//             unitsFormat={unitsFormat}
-//             units={units}
-//           />
-//         </AmountPropertySelectorWrapper>
-//       );
-//     }
-//   };
-// }
-
-function _getValidationMessage(
+function getValidationMessage(
   propertyValueSet: PropertyValueSet.PropertyValueSet,
   value: Amount.Amount<unknown> | undefined,
   validationFilter: PropertyFilter.PropertyFilter,
