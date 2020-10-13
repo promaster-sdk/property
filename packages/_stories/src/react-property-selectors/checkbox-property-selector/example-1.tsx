16f87bf68a76679770618b8192cdd62109b4fee0
Jonas Kello <jonas.kello@gmail.com>
2020-10-13 11:54:45 +0200

    Use hooks in example

--- packages/_stories/src/react-property-selectors/checkbox-property-selector/example-1.tsx
+++ packages/_stories/src/react-property-selectors/checkbox-property-selector/example-1.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable functional/no-this-expression */
-import React from "react";
+import React, { useState } from "react";
 import { BaseUnits, Unit } from "uom";
 import {
   createCheckboxPropertySelector,
@@ -10,75 +10,55 @@ import {
   PropertyValueSet,
   PropertyValue
 } from "@promaster-sdk/property";
-import { merge } from "../utils";
 
 const unitLookup: Unit.UnitLookup = unitString =>
   (BaseUnits as Unit.UnitMap)[unitString];
 
-interface State {
-  readonly propertyValueSet: PropertyValueSet.PropertyValueSet;
-}
-
 const CheckboxPropertySelector = createCheckboxPropertySelector({});
 
-// eslint-disable-next-line functional/no-class
-export class CheckboxPropertySelectorExample1 extends React.Component<
-  {},
-  State
-> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      propertyValueSet: PropertyValueSet.fromString("a=1", unitLookup)
-    };
-  }
+export function CheckboxPropertySelectorExample1(): JSX.Element {
+  const [myState, setMyState] = useState(
+    PropertyValueSet.fromString("a=1", unitLookup)
+  );
+  const valueItems1: ReadonlyArray<CheckboxPropertyValueItem> = [
+    {
+      value: PropertyValue.create("integer", 0),
+      sortNo: 1,
+      text: "unchecked",
+      validationFilter: PropertyFilter.Empty as PropertyFilter.PropertyFilter
+    },
+    {
+      value: PropertyValue.create("integer", 1),
+      sortNo: 2,
+      text: "checked",
+      validationFilter: PropertyFilter.Empty as PropertyFilter.PropertyFilter
+    }
+  ];
 
-  render(): JSX.Element {
-    const valueItems1: Array<CheckboxPropertyValueItem> = [
-      {
-        value: PropertyValue.create("integer", 0),
-        sortNo: 1,
-        text: "unchecked",
-        validationFilter: PropertyFilter.Empty as PropertyFilter.PropertyFilter
-      },
-      {
-        value: PropertyValue.create("integer", 1),
-        sortNo: 2,
-        text: "checked",
-        validationFilter: PropertyFilter.Empty as PropertyFilter.PropertyFilter
-      }
-    ];
-
-    return (
+  return (
+    <div>
+      <div>CheckboxPropertySelector:</div>
+      <div>PropertyValueSet: {PropertyValueSet.toString(myState)}</div>
       <div>
-        <div>CheckboxPropertySelector:</div>
-        <div>
-          PropertyValueSet:{" "}
-          {PropertyValueSet.toString(this.state.propertyValueSet)}
-        </div>
-        <div>
-          <CheckboxPropertySelector
-            propertyName="a"
-            valueItems={valueItems1}
-            propertyValueSet={this.state.propertyValueSet}
-            locked={false}
-            showCodes={true}
-            onValueChange={pv =>
-              this.setState(
-                merge(this.state, {
-                  propertyValueSet: PropertyValueSet.set(
-                    "a",
-                    pv as PropertyValue.PropertyValue,
-                    this.state.propertyValueSet
-                  )
-                })
+        <CheckboxPropertySelector
+          propertyName="a"
+          valueItems={valueItems1}
+          propertyValueSet={myState}
+          locked={false}
+          showCodes={true}
+          onValueChange={pv =>
+            setMyState(
+              PropertyValueSet.set(
+                "a",
+                pv as PropertyValue.PropertyValue,
+                myState
               )
-            }
-            filterPrettyPrint={() => ""}
-            readOnly={false}
-          />
-        </div>
+            )
+          }
+          filterPrettyPrint={() => ""}
+          readOnly={false}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
