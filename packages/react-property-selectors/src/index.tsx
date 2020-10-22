0cabdb1e1052964eca5f2011ad9b2b2819f980ce
Jonas Kello <jonas.kello@gmail.com>
2020-10-22 09:14:12 +0200

    Move

--- packages/react-property-selectors/src/index.tsx
+++ packages/react-property-selectors/src/index.tsx
@@ -1,5 +1,4 @@
-// Hooks
-export * from "./hooks/amount/index";
-export * from "./hooks/textbox";
-export * from "./hooks/discrete";
-export * from "./hooks/properties-selector";
+export * from "./amount/index";
+export * from "./textbox";
+export * from "./discrete";
+export * from "./properties-selector";
