7877e271415b09b019900e2e4b5e531d7549b367
Jonas Kello <jonas.kello@gmail.com>
2021-06-29 15:23:15 +0200

    Move story

--- packages/react-properties-selector/src/__stories__/olle/button.tsx
+++ packages/react-properties-selector/src/__stories__/olle/button.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function Button(props: any): JSX.Element {
-  return <button {...props}>hej</button>;
+  return <button {...props}>hej2</button>;
 }
